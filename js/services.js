ee43309ef9ba43ad6556114f5429a29a2b7116ea
Alex <olivesargreek@gmail.com>
2022-06-01 12:25:11 +0300

    feat: getData, firstRender, getTrends

--- js/services.js
+++ js/services.js
@@ -1,17 +1,23 @@
 const API_KEY = 'de440de8b2c056052dc479379aa4ecd0';
 const BASE_URL = 'https://api.themoviedb.org/3/';
+const LANGUAGE = '&language=ru-RU';
 
-//trending/all/day?api_key='';
+//all/day?api_key='';
 
 const getData = url => {
-    fetch(url)
+    return fetch(url)
     .then(response => {
-        console.lof(response)
-    });
+        if (response.ok) {
+            return response.json()
+        }
+        throw `Что-то пошло не так, ошибка ${response.status}`
+    })
+    .catch(err => console.error());
 };
 
 
 
-export const getTrends = () => {
-    getData();
+export const getTrends = async (type = 'all', period = 'week', page = 1) => {
+    const url = `${BASE_URL}trending/${type}/${period}?api_key=${API_KEY}${LANGUAGE}&page=${page}`
+    return await getData(url);  
 }
